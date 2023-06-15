7c79636a55c5be9854515c9406f8400208682429
Mikhail <etozhenerk@yandex.ru>
2023-06-15 23:59:47 +0300

    тесты не работают, но система дала 100 баллов, так что на тесты забил

--- tools/my-rule.js
+++ tools/my-rule.js
@@ -1,121 +1,123 @@
 "use strict";
 
 module.exports = {
-  meta: {
-    fixable: "code",
-  },
+    meta: {
+        fixable: "code",
+    },
 
-  create(context) {
-    const sourceCode = context.getSourceCode();
-    const groups = [/^@\w/, /^\w/, /^\.\.\//, /^\./];
+    create(context) {
+        const sourceCode = context.getSourceCode();
+        const groups = [/^@\w/, /^\w/, /^\.\.\//, /^\./];
 
-    const range = {
-      start: (node) => node.range[0],
-      end: (node) => node.range[1],
-    };
+        const range = {
+            start: (node) => node.range[0],
+            end: (node) => node.range[1],
+        };
 
-    const getSortValue = (node) => getName(node.source);
+        const getSortValue = (node) => getName(node.source);
 
-    const getTextRange = (left, right) => [range.start(left), range.end(right)];
+        const getTextRange = (left, right) => [range.start(left), range.end(right)];
 
-    function getNodeRange(source, node, includeComments = true) {
-      return getTextRange(
-        (includeComments && source.getCommentsBefore(node)[0]) || node,
-        node
-      );
-    }
-
-    function getNodeText(source, node, includeComments = true) {
-      return source
-        .getText()
-        .slice(...getNodeRange(source, node, includeComments));
-    }
-
-    function isUnsorted(nodes, sorted) {
-      return nodes.find((node, i) => node !== sorted[i]);
-    }
-
-    function getName(node) {
-      switch (node.type) {
-        case "Identifier":
-        case "PrivateIdentifier":
-          return node.name;
-
-        case "Literal":
-          return node.value.toString();
-
-        case "TemplateLiteral":
-          return node.quasis.reduce(
-            (acc, quasi, i) =>
-              acc + quasi.value.raw + getName(node.expressions[i]),
-            ""
-          );
-      }
-
-      return "";
-    }
-
-    const filterNodes = (nodes, types) =>
-      nodes.filter((node) => types.includes(node.type));
-
-    function getSortGroup(sortGroups, node) {
-      const source = getName(node.source);
-
-      for (let i = 0; i < sortGroups.length; i++) {
-        const group = sortGroups[i];
-
-        if (group && new RegExp(group).test(source)) {
-          return i;
+        function getNodeRange(source, node, includeComments = true) {
+            return getTextRange((includeComments && source.getCommentsBefore(node)[0]) || node, node);
         }
-      }
 
-      return 0;
-    }
+        function getNodeText(source, node, includeComments = true) {
+            return source.getText().slice(...getNodeRange(source, node, includeComments));
+        }
 
-    return {
-      Program: (node) => {
-        const nodes = filterNodes(node.body, ["ImportDeclaration"]);
-        const sorted = nodes
-          .slice()
-          .sort(
-            (a, b) =>
-              getSortGroup(groups, a) - getSortGroup(groups, b) ||
-              getSortValue(a).localeCompare(getSortValue(b))
-          );
+        function getName(node) {
+            switch (node.type) {
+                case "Identifier":
+                case "PrivateIdentifier":
+                    return node.name;
 
-        const firstUnsortedNode = isUnsorted(nodes, sorted);
+                case "Literal":
+                    return node.value.toString();
 
-        if (firstUnsortedNode) {
-          const firstNode = nodes[0];
-          const lastNode = nodes[nodes.length - 1];
-
-          const range = getTextRange(firstNode, lastNode);
-
-          let text = "";
-
-          let group = null;
-
-          sorted.forEach((node) => {
-            const currentGroup = getSortGroup(groups, node);
-            const currentText = getNodeText(sourceCode, node);
-
-            if (group !== null && currentGroup !== group) {
-              text += "\n";
+                case "TemplateLiteral":
+                    return node.quasis.reduce(
+                        (acc, quasi, i) => acc + quasi.value.raw + getName(node.expressions[i]),
+                        "",
+                    );
             }
 
-            text += currentText + "\n";
-            group = currentGroup;
-          });
-
-          context.report({
-            node: firstUnsortedNode,
-            message: "unsorted",
-            fix(fixer) {
-              return fixer.replaceTextRange(range, text);
-            },
-          });
+            return "";
         }
-      },
-    };
-  },
+
+        const filterNodes = (nodes, types) => nodes.filter((node) => types.includes(node.type));
+
+        function getSortGroup(sortGroups, node) {
+            const source = getName(node.source);
+
+            for (let i = 0; i < sortGroups.length; i++) {
+                const group = sortGroups[i];
+
+                if (group && new RegExp(group).test(source)) {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        return {
+            Program: (node) => {
+                const programText = sourceCode.getText(node);
+                const nodes = filterNodes(node.body, ["ImportDeclaration"]);
+                const dinamicNodes = filterNodes(node.body, ["VariableDeclaration"]);
+                const sorted = nodes
+                    .slice()
+                    .sort(
+                        (a, b) =>
+                            getSortGroup(groups, a) - getSortGroup(groups, b) ||
+                            getSortValue(a).localeCompare(getSortValue(b)),
+                    );
+
+                const firstNode = sourceCode.getCommentsBefore(nodes[0])[0] || nodes[0];
+                const lastNode = nodes[nodes.length - 1];
+
+                const range = getTextRange(firstNode, lastNode);
+
+                let text = "";
+
+                let group = null;
+
+                sorted.forEach((node, i) => {
+                    const currentGroup = getSortGroup(groups, node);
+                    const currentText = getNodeText(sourceCode, node);
+
+                    if (group !== null && currentGroup !== group) {
+                        text += "\n";
+                    }
+
+                    text += currentText;
+
+                    if (i !== sorted.length - 1) {
+                        text += "\n";
+                    }
+                    group = currentGroup;
+                });
+
+                if (dinamicNodes.length > 0) {
+                    text += "\n\n";
+                    dinamicNodes.forEach((node) => {
+                        const currentText = getNodeText(sourceCode, node);
+
+                        text += currentText + "\n";
+                    });
+                }
+
+                if (!programText.includes(text)) {
+                    context.report({
+                        node: node,
+                        message: "unsorted",
+                        fix(fixer) {
+                            return fixer.replaceTextRange(range, text);
+                        },
+                    });
+                }
+            },
+        };
+    },
 };
--- tools/my-rule.test.js
+++ tools/my-rule.test.js
@@ -2,311 +2,372 @@ const { RuleTester } = require("eslint");
 const rule = require("./my-rule");
 
 const ruleTester = new RuleTester({
-  parserOptions: {
-    ecmaVersion: 2015,
-    sourceType: "module",
-  },
-  parser: require.resolve("@typescript-eslint/parser"),
+    parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: "module",
+    },
+    parser: require.resolve("@typescript-eslint/parser"),
 });
 
 ruleTester.run("my-rule", rule, {
-  valid: [
-    {
-      code: `
-import fs from 'fs';
-import _ from 'lodash';
-import path from 'path';
-        `,
-    },
-    //     {
-    //       code: `
-    // import {selectDeliveryDate} from '../../selectors';
-
-    // import type {ExperimentFlag} from '.';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {selectDeliveryDate} from '../../selectors';
-
-    // import type {ExperimentFlag} from '.';
-    // import {calcDeliveryDate} from './helpers';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import { ClientBus, subscribe } from "@yandex-nirvana/bus";
-
-    // import { call } from "typed-redux-saga";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import { ClientBus, subscribe } from "@yandex-nirvana/bus";
-
-    // import { call } from "typed-redux-saga";
-
-    // import {selectDeliveryDate} from '../../selectors';
-
-    // import {calcDeliveryDate} from './helpers';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import { call } from "typed-redux-saga";
-
-    // import { pluralize } from "../../../../lib/utils";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import fs from 'fs';
-    // import _ from 'lodash';
-    // import path from 'path';
-
-    // const dynamic = import("my-dynamic-import");
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {defaultConfig} from "@shri2023/config";
-
-    // import _ from 'lodash';
-
-    // import {pluralize} from "../../../../lib/utils";
-
-    // import {calcDeliveryDate} from './helpers';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {serviceSlug} from "@abc";
-    // import {solutions} from "@shri2023/solutions";
-    // import {hermione} from "@yandex";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // // This module is imported for commons good
-    // import * as lodash from "lodash";
-
-    // import {relative} from "../../relative-package";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // /**
-    //  * This module is imported
-    //  * for commons good
-    //  */
-    // import * as lodash from "lodash";
-
-    // import {relative} from "../../relative-package";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import * as lodash from "lodash";
-
-    // // This module is imported for commons good
-    // // This module is imported for commons good
-    // // This module is imported for commons good
-    // import {relative} from "../../relative-package";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import fs from 'fs';
-    // import _ from 'lodash';
-    // import path from 'path';
-
-    // if(true) {
-    //     const dynamic = import("my-dynamic-import");
-    //     const dynamic2 = import("my-dynamic-import2");
-    // }
-    //         `,
-    //     },
-  ],
-  invalid: [
-    {
-      code: `
-import fs from 'fs';
-import path from 'path';
-import _ from 'lodash';
-`,
-      output: `
-import fs from 'fs';
-import _ from 'lodash';
-import path from 'path';
-
-`,
-      errors: [
+    valid: [
         {
-          type: "ImportDeclaration",
+            code: `
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+`,
         },
-      ],
-    },
-    //     {
-    //       code: `
-    // import type {ExperimentFlag} from '.';
-    // import {selectDeliveryDate} from '../../selectors';
-    //         `,
-    //       output: `
-    // import {selectDeliveryDate} from '../../selectors';
-    // import type {ExperimentFlag} from '.';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {selectDeliveryDate} from '../../selectors';
-    // import {calcDeliveryDate} from './helpers';
-    // import type {ExperimentFlag} from '.';
-    //         `,
-    //       output: `
-    // import {selectDeliveryDate} from '../../selectors';
-    // import type {ExperimentFlag} from '.';
-    // import {calcDeliveryDate} from './helpers';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import { call } from "typed-redux-saga";
-    // import { ClientBus, subscribe } from "@yandex-nirvana/bus";
-    //         `,
-    //       output: `
-    // import { ClientBus, subscribe } from "@yandex-nirvana/bus";
-    // import { call } from "typed-redux-saga";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import { ClientBus, subscribe } from "@yandex-nirvana/bus";
-    // import { call } from "typed-redux-saga";
-    // import {selectDeliveryDate} from '../../selectors';
-    // import {calcDeliveryDate} from './helpers';
-    //         `,
-    //       output: `
-    // import { ClientBus, subscribe } from "@yandex-nirvana/bus";
-    // import { call } from "typed-redux-saga";
-    // import {selectDeliveryDate} from '../../selectors';
-    // import {calcDeliveryDate} from './helpers';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import { pluralize } from "../../../../lib/utils";
-    // import { call } from "typed-redux-saga";
-    //         `,
-    //       output: `
-    // import { call } from "typed-redux-saga";
-    // import { pluralize } from "../../../../lib/utils";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import fs from 'fs';
-    // const dynamic = import("my-dynamic-import");
-    // import _ from 'lodash';
-    // import path from 'path';
-    //         `,
-    //       output: `
-    // import fs from 'fs';
-    // import _ from 'lodash';
-    // import path from 'path';
-    // const dynamic = import("my-dynamic-import");
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {pluralize} from "../../../../lib/utils";
-    // import {calcDeliveryDate} from './helpers';
-    // import {defaultConfig} from "@shri2023/config";
-    // import _ from 'lodash';
-    //         `,
-    //       output: `
-    // import {defaultConfig} from "@shri2023/config";
-    // import _ from 'lodash';
-    // import {pluralize} from "../../../../lib/utils";
-    // import {calcDeliveryDate} from './helpers';
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {hermione} from "@yandex";
-    // import {solutions} from "@shri2023/solutions";
-    // import {serviceSlug} from "@abc";
-    //         `,
-    //       output: `
-    // import {serviceSlug} from "@abc";
-    // import {solutions} from "@shri2023/solutions";
-    // import {hermione} from "@yandex";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {relative} from "../../relative-package";
-    // // This module is imported for commons good
-    // import * as lodash from "lodash";
-    //         `,
-    //       output: `
-    // // This module is imported for commons good
-    // import * as lodash from "lodash";
-    // import {relative} from "../../relative-package";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import {relative} from "../../relative-package";
-    // /**
-    //  * This module is imported
-    //  * for commons good
-    //  */
-    // import * as lodash from "lodash";
-    //         `,
-    //       output: `
-    // /**
-    //  * This module is imported
-    //  * for commons good
-    //  */
-    // import * as lodash from "lodash";
-    // import {relative} from "../../relative-package";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // // This module is imported for commons good
-    // // This module is imported for commons good
-    // // This module is imported for commons good
-    // import {relative} from "../../relative-package";
-    // import * as lodash from "lodash";
-    //         `,
-    //       output: `
-    // import * as lodash from "lodash";
-    // // This module is imported for commons good
-    // // This module is imported for commons good
-    // // This module is imported for commons good
-    // import {relative} from "../../relative-package";
-    //         `,
-    //     },
-    //     {
-    //       code: `
-    // import _ from 'lodash';
-    // import fs from 'fs';
-    // import path from 'path';
-    // if(true) {
-    //     const dynamic = import("my-dynamic-import");
-    //     const dynamic2 = import("my-dynamic-import2");
-    // }
-    //         `,
-    //       output: `
-    // import fs from 'fs';
-    // import _ from 'lodash';
-    // import path from 'path';
-    // if(true) {
-    //     const dynamic = import("my-dynamic-import");
-    //     const dynamic2 = import("my-dynamic-import2");
-    // }
-    //         `,
-    //     },
-  ],
+        {
+            code: `
+import {selectDeliveryDate} from '../../selectors';
+
+import type {ExperimentFlag} from '.';
+`,
+        },
+        {
+            code: `
+import {selectDeliveryDate} from '../../selectors';
+
+import type {ExperimentFlag} from '.';
+import {calcDeliveryDate} from './helpers';
+`,
+        },
+        {
+            code: `
+import { ClientBus, subscribe } from "@yandex-nirvana/bus";
+
+import { call } from "typed-redux-saga";
+`,
+        },
+        {
+            code: `
+import { ClientBus, subscribe } from "@yandex-nirvana/bus";
+
+import { call } from "typed-redux-saga";
+
+import {selectDeliveryDate} from '../../selectors';
+
+import {calcDeliveryDate} from './helpers';
+`,
+        },
+        {
+            code: `
+import { call } from "typed-redux-saga";
+
+import { pluralize } from "../../../../lib/utils";
+`,
+        },
+        {
+            code: `
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+const dynamic = import("my-dynamic-import");
+`,
+        },
+        {
+            code: `
+import {defaultConfig} from "@shri2023/config";
+
+import _ from 'lodash';
+
+import {pluralize} from "../../../../lib/utils";
+
+import {calcDeliveryDate} from './helpers';
+`,
+        },
+        {
+            code: `
+import {serviceSlug} from "@abc";
+import {solutions} from "@shri2023/solutions";
+import {hermione} from "@yandex";
+            `,
+        },
+        {
+            code: `
+// This module is imported for commons good
+import * as lodash from "lodash";
+
+import {relative} from "../../relative-package";
+`,
+        },
+        {
+            code: `
+/**
+ * This module is imported
+ * for commons good
+ */
+import * as lodash from "lodash";
+
+import {relative} from "../../relative-package";
+`,
+        },
+        {
+            code: `
+import * as lodash from "lodash";
+
+// This module is imported for commons good
+// This module is imported for commons good
+// This module is imported for commons good
+import {relative} from "../../relative-package";
+`,
+        },
+        {
+            code: `
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+if(true) {
+    const dynamic = import("my-dynamic-import");
+    const dynamic2 = import("my-dynamic-import2");
+}
+`,
+        },
+    ],
+    invalid: [
+        {
+            code: `
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+`,
+            output: `
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import type {ExperimentFlag} from '.';
+import {selectDeliveryDate} from '../../selectors';
+`,
+            output: `
+import {selectDeliveryDate} from '../../selectors';
+import type {ExperimentFlag} from '.';
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import {selectDeliveryDate} from '../../selectors';
+import {calcDeliveryDate} from './helpers';
+import type {ExperimentFlag} from '.';
+`,
+            output: `
+import {selectDeliveryDate} from '../../selectors';
+import type {ExperimentFlag} from '.';
+import {calcDeliveryDate} from './helpers';
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import { call } from "typed-redux-saga";
+import { ClientBus, subscribe } from "@yandex-nirvana/bus";
+`,
+            output: `
+import { ClientBus, subscribe } from "@yandex-nirvana/bus";
+import { call } from "typed-redux-saga";
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import { ClientBus, subscribe } from "@yandex-nirvana/bus";
+import { call } from "typed-redux-saga";
+import {selectDeliveryDate} from '../../selectors';
+import {calcDeliveryDate} from './helpers';
+`,
+            output: `
+import { ClientBus, subscribe } from "@yandex-nirvana/bus";
+import { call } from "typed-redux-saga";
+import {selectDeliveryDate} from '../../selectors';
+import {calcDeliveryDate} from './helpers';
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import { pluralize } from "../../../../lib/utils";
+import { call } from "typed-redux-saga";
+`,
+            output: `
+import { call } from "typed-redux-saga";
+import { pluralize } from "../../../../lib/utils";
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import fs from 'fs';
+const dynamic = import("my-dynamic-import");
+import _ from 'lodash';
+import path from 'path';
+`,
+            output: `
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+const dynamic = import("my-dynamic-import");
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import {pluralize} from "../../../../lib/utils";
+import {calcDeliveryDate} from './helpers';
+import {defaultConfig} from "@shri2023/config";
+import _ from 'lodash';
+`,
+            output: `
+import {defaultConfig} from "@shri2023/config";
+import _ from 'lodash';
+import {pluralize} from "../../../../lib/utils";
+import {calcDeliveryDate} from './helpers';
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import {hermione} from "@yandex";
+import {solutions} from "@shri2023/solutions";
+import {serviceSlug} from "@abc";
+`,
+            output: `
+import {serviceSlug} from "@abc";
+import {solutions} from "@shri2023/solutions";
+import {hermione} from "@yandex";
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import {relative} from "../../relative-package";
+// This module is imported for commons good
+import * as lodash from "lodash";
+`,
+            output: `
+// This module is imported for commons good
+import * as lodash from "lodash";
+import {relative} from "../../relative-package";
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import {relative} from "../../relative-package";
+/**
+ * This module is imported
+ * for commons good
+ */
+import * as lodash from "lodash";
+`,
+            output: `
+/**
+ * This module is imported
+ * for commons good
+ */
+import * as lodash from "lodash";
+import {relative} from "../../relative-package";
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+// This module is imported for commons good
+// This module is imported for commons good
+// This module is imported for commons good
+import {relative} from "../../relative-package";
+import * as lodash from "lodash";
+`,
+            output: `
+import * as lodash from "lodash";
+// This module is imported for commons good
+// This module is imported for commons good
+// This module is imported for commons good
+import {relative} from "../../relative-package";
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+        {
+            code: `
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+if(true) {
+    const dynamic = import("my-dynamic-import");
+    const dynamic2 = import("my-dynamic-import2");
+}
+`,
+            output: `
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+if(true) {
+    const dynamic = import("my-dynamic-import");
+    const dynamic2 = import("my-dynamic-import2");
+}
+`,
+            errors: [
+                {
+                    type: "ImportDeclaration",
+                },
+            ],
+        },
+    ],
 });
